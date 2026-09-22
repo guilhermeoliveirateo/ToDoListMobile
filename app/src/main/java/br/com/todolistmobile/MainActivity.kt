780fb58a5972e303bbb1b9d138dc24be00593a77
Guilherme Teodoro de Oliveira <guilhermeoliveirateo@gmail.com>
2026-09-22 19:13:40 +0100

    feat: start developing interface for to-do list mobile app

--- app/src/main/java/br/com/todolistmobile/MainActivity.kt
+++ app/src/main/java/br/com/todolistmobile/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.todolistmobile.ui.theme.ToDoListMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,55 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ToDoListMobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ToDoList()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ToDoList() {
+
+    var task by remember { mutableStateOf(value = "") }
+
+    fun addTask() {
+
+    }
+
+    fun completeTask() {
+
+    }
+
+    fun clearTasks() {
+
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(space = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "To Do List", style = MaterialTheme.typography.titleLarge)
+
+        OutlinedTextField(
+            value = task,
+            onValueChange = { v -> task = v.filter { it.isDigit() || it.isLetter()} },
+            label = { Text(text = "Type a task") },
+            trailingIcon = {
+                IconButton(onClick = {addTask()}) {
+                    Text(text = "Add")
+                }
+            },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToDoListMobileTheme {
-        Greeting("Android")
+        ToDoList()
     }
 }
